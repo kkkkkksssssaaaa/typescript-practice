5107dcb16779abdb6d40dc13ac2aaa7b8525443d
kkkkkksssssaaaa <kofc312@naver.com>
2022-12-28 22:52:56 +0900

    test(game-number): add test case

---
+++ __tests__/game/game-number.spec.ts
@@ -0,0 +1,27 @@
+import { GameNumber } from '../../src/game/game-number';
+
+describe('game-number', () => {
+  test('범위 내에 속하는 값으로 인자로 보내 초기화할 수 있다', async () => {
+    await expect(() => GameNumber.of(5)).not.toThrow();
+  });
+
+  test('경계값 1로 초기화할 수 있다', async () => {
+    await expect(() => GameNumber.of(1)).not.toThrow();
+  });
+
+  test('경계값 9로 초기화할 수 있다', async () => {
+    await expect(() => GameNumber.of(1)).not.toThrow();
+  });
+
+  test('범위를 벗어난 인자로 초기화할 수 없다', async () => {
+    await expect(() => GameNumber.of(15)).toThrowError
+  });
+
+  test('음수로 초기화할 수 없다', async () => {
+    await expect(() => GameNumber.of(-1)).toThrowError
+  });
+
+  test('0으로 초기화할 수 없다', async () => {
+    await expect(() => GameNumber.of(0)).toThrowError
+  });
+})

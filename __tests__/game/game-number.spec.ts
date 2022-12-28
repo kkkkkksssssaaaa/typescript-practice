f500f62c3abf23baf20d9f043b4f1a55fd147379
kkkkkksssssaaaa <kofc312@naver.com>
2022-12-28 22:57:03 +0900

    test(game-number): add get value test

--- __tests__/game/game-number.spec.ts
+++ __tests__/game/game-number.spec.ts
@@ -14,14 +14,20 @@ describe('game-number', () => {
   });
 
   test('범위를 벗어난 인자로 초기화할 수 없다', async () => {
-    await expect(() => GameNumber.of(15)).toThrowError
+    await expect(() => GameNumber.of(15)).toThrowError();
   });
 
   test('음수로 초기화할 수 없다', async () => {
-    await expect(() => GameNumber.of(-1)).toThrowError
+    await expect(() => GameNumber.of(-1)).toThrowError();
   });
 
   test('0으로 초기화할 수 없다', async () => {
-    await expect(() => GameNumber.of(0)).toThrowError
+    await expect(() => GameNumber.of(0)).toThrowError();
   });
-})
+});
+
+describe('game-number.get()', () => {
+  test('인스턴스에서 값을 가져올 수 있다', async () => {
+    await expect(GameNumber.of(1).get()).toBe(1);
+  });
+});

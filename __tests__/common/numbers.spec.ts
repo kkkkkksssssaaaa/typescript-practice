f4d044cc4a9bde156fd228ccd236864610377492
kkkkkksssssaaaa <kofc312@naver.com>
2022-12-29 16:22:08 +0900

    test(numbers): add test case

---
+++ __tests__/common/numbers.spec.ts
@@ -0,0 +1,23 @@
+import { Numbers } from "../../src/common/numbers";
+
+describe('numbers.isMoreThanMax', () => {
+  test('음수를 인자로 넘기면 false 를 반환한다', async () => {
+    await expect(Numbers.isMoreThanMax(-1)).toEqual(false);
+  });
+
+  test('1을 인자로 넘기면 false 를 반환한다', async () => {
+    await expect(Numbers.isMoreThanMax(1)).toEqual(false);
+  });
+
+  test('최대값보다 작은 수를 인자로 넘기면 false 를 반환한다', async () => {
+    await expect(Numbers.isMoreThanMax(5)).toEqual(false);
+  });
+
+  test('최대값을 인자로 넘기면 false 를 반환한다', async () => {
+    await expect(Numbers.isMoreThanMax(9)).toEqual(false);
+  });
+
+  test('최대값보다 큰 수를 인자로 넘기면 true 를 반환한다', async () => {
+    await expect(Numbers.isMoreThanMax(10)).toEqual(true);
+  });
+});

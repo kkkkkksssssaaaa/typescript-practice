ccd45e35bc094e715871e3761e7448a22a5d20c2
kkkkkksssssaaaa <kofc312@naver.com>
2023-01-04 23:51:26 +0900

    feat(game-numbers): add constructor validation

--- src/game/game-numbers.ts
+++ src/game/game-numbers.ts
@@ -2,9 +2,13 @@ import { GameNumber } from "./game-number";
 
 export class GameNumbers {
 
+  private static readonly LENGTH: Number = 3;
+  
   private readonly values: GameNumber[];
 
   private constructor(values: GameNumber[]) {
+    this.checkLength(values);
+
     this.values = values;
   }
 
@@ -14,5 +18,11 @@ export class GameNumbers {
 
   public toArray(): GameNumber[] {
     return this.values;
+  }
+
+  private checkLength(values: GameNumber[]): void {
+    if (values.length !== 3) {
+      throw new Error();
+    }
   }
 }
